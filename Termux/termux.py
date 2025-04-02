1ae48685117e9d9f6c1d7bf2fc48beaaa469e58c
furry-onko <cute.neko.boy.uwu@gmail.com>
2025-04-02 15:51:00 +0200

    repaired colors and added more commands

--- Termux/termux.py
+++ Termux/termux.py
@@ -1,8 +1,8 @@
 import os
 import sys
-import colors.py as c
+import colors as c
 
-def Insert() -> None;
+def Insert() -> None:
     while True:
         cmd_input: str|int = input(c.color(">>", "green"))
         cmd: list = cmd_input.split(" ")
@@ -21,18 +21,41 @@ def Insert() -> None;
         if cmd[0] in ["clear", "cl", "~"]:
             print("\033[H\033[J", end="")
             continue
-        
+
         if cmd[0] in ["help", "?"]:
-            print(style(color("Commands:", "purple"), "bold"))
-            print(style(color("Syntax:", "cyan"), "bold"))
-            # <required_argument> [optional_argument] (option1|option2)
-            print(color("required_command", ""))
+            print(c.style(c.color("Commands:", "purple"), "bold"))
+            
+            print(c.style(c.color("Syntax:", "cyan"), "bold"))
+            print(c.color("required_command", "red"), end=" ")
+            print(c.color("[optional_argument]", "yellow"), end=" ")
+            print(c.color("<required_argument>", "green"), end=" ")
+            print(c.color("(option1|option2)", "cyan"))
+
+            print("-" * 50)
+
+            print(c.color("(exit|quit|#)", "cyan"), end=" ")
+            print(c.color("[exit_code]", "yellow"), end=" ")
+            print("Quit the program")
+
+            print(c.color("(clear|cls|~)", "cyan"), end=" ")
+            print("Clear the screen")
+
+            print(c.color("(help|?)", "cyan"), end=" ")
+            print("Show this help message")
+
+            print(c.color("!", "red"), end=" ")
+            print(c.color("<command>", "green"), end=" ")
+            print("Execute command as administrator")
+
+
 
         else:
-            print(color(f"Command {cmd[0]} is not recognized as Termux command", "red"))
+            print(c.color(f"Command {cmd[0]} is not recognized as Termux command", "red"))
 
 def main():
-    print(color("Termux ", "purple"), end="")
-    print(color("v1.0.0", "cyan"))
-    print(style(color("By: Onko Aikuu (@furry_onko)", "blue"), "bold"))
+    print(c.color("Termux ", "purple"), end="")
+    print(c.color("v1.0.0", "cyan"))
+    print(c.style(c.color("By: Onko Aikuu (@furry_onko)", "blue"), "bold"))
     Insert()
+
+main()
